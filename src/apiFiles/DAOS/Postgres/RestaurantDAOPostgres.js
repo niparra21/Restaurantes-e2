647be1bd3b8dd4517a4388719491050c8d284fac
Mariann44 <vamarin@estudiantec.cr>
2025-04-26 17:13:55 -0600

    DAO initial configuration
    no entiendo nada, besos <3

---
+++ src/apiFiles/DAOS/Postgres/RestaurantDAOPostgres.js
@@ -0,0 +1,33 @@
+const pool = require('../../db');
+
+class RestaurantDAOPostgres {
+    
+    async registerRestaurant(name, addres, phone, owner_id){
+        try {
+            const result = await pool.query(
+                `INSERT INTO restaurants (name, address, phone, owner_id, created_at, updated_at)
+                 VALUES ($1, $2, $3, $4, NOW(), NOW())
+                RETURNING *`,
+            [name, address, phone, owner_id]
+            );
+            return result.rows[0];
+        } catch (error) {
+            console.error("Error al registrar restaurante:", error);
+            throw error;
+        }
+        
+    }
+
+    async getRestaurants() {
+        try {
+            const result = await pool.query('SELECT id, name, address, phone, owner_id FROM restaurants');
+            return result.rows;
+        } catch (error) {
+            console.error("Error al obtener restaurantes:", error);
+            throw error;
+        }
+    }
+
+}
+
+module.exports = new RestaurantDAOPostgres();
---
+++ src/apiFiles/dbMongo.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: __dirname + '/../.env' });
+const { MongoClient } = require('mongodb');
+
+const uri = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DB_HOST}:27017`;
+
+const client = new MongoClient(uri);
+
+const connectMongoDB = async () => {
+    try {
+        await client.connect();
+        console.log('Conexión exitosa a MongoDB');
+        return client.db(process.env.MONGO_INITDB_DATABASE);
+    } catch (err) {
+        console.error('Error al conectar a MongoDB:', err.message);
+    }
+};
+
+module.exports = connectMongoDB;
